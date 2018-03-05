c004daea41b7f27e574a948fbbc7f11606c8fdb1
Goba <36506501+goba2000@users.noreply.github.com>
2018-03-05 10:31:25 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,20 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
   pushButton.textContent = 'Push Not Supported';
 }
 
+'use strict';
+const applicationServerPublicKey = 'BFIKR7D7sp-v4ogJVEh7Y1_RO7rVs1hM5tRDAEOfeS0Zo-l7Rrvqf7vI_hRPWfpg2ZGfD9F81ENtgKx_05T95C4';
+const pushButton = document.querySelector('.js-push-btn');
+let isSubscribed = false;
+let swRegistration = null;
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
