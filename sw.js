be0fdfa5bca3b62d5ff174be572a5f64b1fb2af0
Goba <36506501+goba2000@users.noreply.github.com>
2018-03-06 09:51:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -21,36 +21,15 @@
 
 'use strict';
 
-/* eslint-disable max-len */
-
-const applicationServerPublicKey = 'BHexfq_wOBgjOHyB2eqsYlXJLuLSnDbhe0qMNgMFdLQzf-dHaSBqyIdA-XZU1VfZdpm_7yXAfZxsLIZGdP0xgYk';
-
-/* eslint-enable max-len */
-
-function urlB64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
-
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
 self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Push Codelab';
+  const title = 'Push Сообщение:';
   const options = {
-    body: 'Yay it works.',
-    icon: 'images/icon.png',
-    badge: 'images/badge.png'
+    body: 'Privet GOBA !!!! 2018 Хорошего Дня!',
+    icon: 'push/images/icon.png',
+    badge: 'push/images/badge.png'
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
@@ -63,20 +42,5 @@ self.addEventListener('notificationclick', function(event) {
 
   event.waitUntil(
     clients.openWindow('https://developers.google.com/web/')
-  );
-});
-
-self.addEventListener('pushsubscriptionchange', function(event) {
-  console.log('[Service Worker]: \'pushsubscriptionchange\' event fired.');
-  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
-  event.waitUntil(
-    self.registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: applicationServerKey
-    })
-    .then(function(newSubscription) {
-      // TODO: Send to application server
-      console.log('[Service Worker] New subscription: ', newSubscription);
-    })
   );
 });
